d217aadd041364399866b91dc7855099ee82585b
Kenura-R-Gunarathna <kenuragunarathna@gmail.com>
2023-09-26 20:37:38 +0530

    add changes

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,12 +1,22 @@
 import Header from '@/components/molecules/HeaderMolecule'
 import NavbarMolecule from '@/components/molecules/NavbarMolecule'
 import SideNavbarMolecule from '@/components/molecules/SideNavbarMolecule'
+import Stepper from "@/components/molecules/Stepper";
+import StepperControl from "@/components/molecules/StepperControl";
 
 import { useState, useEffect } from 'react';
+import { StepperContext } from "../../context/StepperContext";
+
+import Question from "../../components/molecules/steps/Question";
+import Teacher from "../../components/molecules/steps/Teacher";
+import Final from "../../components/molecules/steps/Final";
 
 export default function Dashboard() {
 
   const [windowHeight, setWindowHeight] = useState(0);
+  const [currentStep, setCurrentStep] = useState(1);
+  const [userData, setUserData] = useState("");
+  const [finalData, setFinalData] = useState([]);
 
   useEffect(() => {
 
@@ -23,6 +33,28 @@ export default function Dashboard() {
     };
   });
 
+  const steps = ["Question", "Teacher", "Complete"];
+
+  const displayStep = (step) => {
+    switch (step) {
+      case 1:
+        return <Question />;
+      case 2:
+        return <Teacher />;
+      case 3:
+        return <Final />;
+      default:
+    }
+  };
+
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+
+    direction === "next" ? newStep++ : newStep--;
+    // check steps are withing bounds
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  };
+
   return (
     <>
 
@@ -38,7 +70,43 @@ export default function Dashboard() {
 
           <div className="container">
 
-            Dashboard Page
+            <div className="grid grid-cols-4">
+
+              <div className="flex flex-col justify-center mt-4 pb-2 col-span-4  lg:col-span-3">
+                {/* Stepper */}
+                <div className="container mt-5 horizontal">
+                  <Stepper steps={steps} currentStep={currentStep} />
+
+                  {/* Display Component */}
+                  <div className="my-10 p-10">
+                    <StepperContext.Provider
+                      value={{
+                        userData,
+                        setUserData,
+                        finalData,
+                        setFinalData,
+                      }}
+                    >
+                      {displayStep(currentStep)}
+                    </StepperContext.Provider>
+                  </div>
+                </div>
+
+                {/* Navigation */}
+                {currentStep !== steps.length && (
+                  <StepperControl
+                    handleClick={handleClick}
+                    currentStep={currentStep}
+                    step={steps}
+                  />
+                )}
+              </div>
+
+              <div className="col-span-4  lg:col-span-1">
+                Side Data
+              </div>
+
+            </div>
 
           </div>
 sd
--- src/pages/forum/index.jsx
+++
@@ -1,3 +0,0 @@
-export default function Forum() {
-    return <div>Forum</div>
-}
