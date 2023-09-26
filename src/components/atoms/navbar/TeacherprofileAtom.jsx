4b0704925a31c7e56a75ac56580c17c0f199ed02
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2023-09-26 14:55:11 +0530

    teacher profile
    added intro video section and answered questions section

--- src/components/atoms/navbar/TeacherprofileAtom.jsx
+++ src/components/atoms/navbar/TeacherprofileAtom.jsx
@@ -1,9 +1,3 @@
-{
-  /*
-https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
-*/
-}
-
 import {
   AiFillStar,
   AiOutlineStar,
@@ -12,9 +6,12 @@ import {
   AiFillFacebook,
 } from "react-icons/ai";
 
+import { FaCircle, FaEllipsisV } from "react-icons/fa";
+
 export default function TeacherProfile(props) {
   return (
     <>
+      {/* Hero section */}
       <section className="pt-0 mb-4">
         <div className="container px-5 pt-14">
           <div className="md:flex justify-between items-center sm:flex-col md:flex-row">
@@ -116,11 +113,118 @@ export default function TeacherProfile(props) {
         </div>
       </section>
 
-      {/* Subjects */}
-      <section> Subjects goes here </section>
+      {/* About teacher */}
+      <section className="flex justify-center items-center flex-col w-full max-w-5xl mx-auto">
+        <div className="tracking-[1px] my-[30px] py-[10px] px-5 flex flex-col justify-center items-center">
+          <span className="text-text-color font-[300] text-md tracking-[2px] uppercase">
+            INTRODUCTION
+          </span>
+          <h1 className="text-text-color font-[700] text-[32px]">About me</h1>
+        </div>
 
-      {/* Ansered Questions */}
-      <section> Ansered Questions should go here </section>
+        <div className="max-w-5xl flex flex-row flex-wrap justify-center items-center">
+          <div className="w-full md:w-1/2">
+            {/* Content goes here */}
+            <p className="text-center md:text-left">
+              I am a software engineering undergraduate at University of Colombo
+              School of Computing. I have a passion for teaching and I have been
+              teaching for the past 3 years. I have been teaching students for
+              the past 3 years and I have been able to help them achieve their
+              goals. I have been teaching students for the past 3 years and I
+              have been able to help them achieve their goals.
+            </p>
+          </div>
+          <div className="w-full md:w-1/2 flex justify-center items-center mt-5 md:mt-0">
+            {/* Video goes here */}
+            <video
+              width="500px"
+              height="500px"
+              controls="controls"
+              className="rounded-xl"
+            >
+              <source
+                src="https://pixabay.com/videos/the-lessons-are-over-teacher-folder-11862/"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+        </div>
+      </section>
+
+      {/* Answered Questions */}
+      <section className="flex justify-center items-center flex-col w-full max-w-5xl mx-auto">
+        <div className="tracking-[1px] my-[30px] py-[10px] px-5 flex flex-col justify-center items-center">
+          <span className="text-text-color font-[300] text-md tracking-[2px] uppercase">
+            ON QUESTIONWISE
+          </span>
+          <h1 className="text-text-color font-[700] text-[32px]">My work</h1>
+        </div>
+
+        <div className="w-full md:w-2/3">
+
+          {/* card for questions */}
+          <div className="px-5 py-5 rounded-lg shadow-[2px_2px_30px_rgba(0,0,0,0.1)]">
+            <div className="flex justify-between items-cente ">
+              <div className="flex gap-4 justify-center items-center">
+                <div className="">
+                  <img
+                    src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80"
+                    alt="student"
+                    className="w-12 h-12 object-cover object-center rounded-full"
+                  />
+                </div>
+                <div>
+                  <div>Kaviru Happuarachchi</div>
+                  <div className="flex justify-start items-center gap-2">
+                    <div>@ksrx</div>
+                    <div>
+                      <FaCircle fill="#444444" className="h-1 w-1 opacity-40" />
+                    </div>
+                    <div className="opacity-40 text-sm">2 days ago</div>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden">
+                <FaEllipsisV fill="#444444" />
+              </div>
+            </div>
+            <div>
+              <div className="flex flex-col mt-5 mb-5">
+                <h2 className="font-bold">How to patch KDE on FreeBSD?</h2>
+                <p className="mt-5">
+                  How to patch KDE on FreeBSD? How to patch KDE on FreeBSD? How
+                  to patch KDE on FreeBSD? How to patch KDE on FreeBSD? How to
+                  patch KDE on FreeBSD? How to patch KDE on FreeBSD? How to
+                  patch KDE on FreeBSD? How to patch KDE on FreeBSD? How to
+                  patch KDE on
+                </p>
+              </div>
+
+              <div className="mb-5">
+                Enter the video/ image or code snippet here
+              </div>
+
+              <div className="mb-5">Enter the code snippet here</div>
+            </div>
+
+            <div className="flex justify-between items-center mx-5">
+              <div className="flex gap-1 flex-wrap">
+                <button className="bg-[#4584b6] text-white rounded-lg px-3 py-1 text-xs">
+                  #Python
+                </button>
+                <button className="bg-[#ffde57] text-text-color rounded-lg px-3 py-1 text-xs">
+                  #JavaScript
+                </button>
+              </div>
+              <div>
+                <button className="bg-primary text-white font-[500] flex items-center gap-2 hover:bg-text-color ease-in duration-300 py-2 rounded-xl px-4">
+                  View Answer
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* Rating */}
       <section className="flex justify-center items-center flex-col w-full max-w-5xl mx-auto">
@@ -134,7 +238,6 @@ export default function TeacherProfile(props) {
         </div>
 
         <div className="flex justify-center items-center flex-wrap">
-
           {/* Review 01 */}
           <div className="flex justify-center items-center flex-wrap md:w-[50%] w-full">
             <div className="width-[500px] shadow-[2px_2px_30px_rgba(0,0,0,0.1)] bg-white p-5 m-4 cursor-pointer hover:scale-110 transition duration-200 ease-in-out">
@@ -152,9 +255,7 @@ export default function TeacherProfile(props) {
                     <h4 className="text-text-color text-[1.1rem] leading-[0.5px] mb-1">
                       Kaviru Happuarachchi
                     </h4>
-                    <span className="text-[#979797] text-[0.8rem]">
-                      @ksrx
-                    </span>
+                    <span className="text-[#979797] text-[0.8rem]">@ksrx</span>
                   </div>
                 </div>
 
@@ -193,9 +294,7 @@ export default function TeacherProfile(props) {
                     <h4 className="text-text-color text-[1.1rem] leading-[0.5px] mb-1">
                       Kaviru Happuarachchi
                     </h4>
-                    <span className="text-[#979797] text-[0.8rem]">
-                      @ksrx
-                    </span>
+                    <span className="text-[#979797] text-[0.8rem]">@ksrx</span>
                   </div>
                 </div>
 
@@ -234,9 +333,7 @@ export default function TeacherProfile(props) {
                     <h4 className="text-text-color text-[1.1rem] leading-[0.5px] mb-1">
                       Kaviru Happuarachchi
                     </h4>
-                    <span className="text-[#979797] text-[0.8rem]">
-                      @ksrx
-                    </span>
+                    <span className="text-[#979797] text-[0.8rem]">@ksrx</span>
                   </div>
                 </div>
 
