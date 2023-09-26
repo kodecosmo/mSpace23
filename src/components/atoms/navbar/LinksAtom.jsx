680506c8355d7d3ccb85a24a585840d6856b9f48
Kenura-R-Gunarathna <kenuragunarathna@gmail.com>
2023-09-26 21:34:35 +0530

    removed Link instead of a

--- src/components/atoms/navbar/LinksAtom.jsx
+++ src/components/atoms/navbar/LinksAtom.jsx
@@ -2,13 +2,13 @@ export default function LinksAtom() {
     return (
         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"aria-current="page" >Home</a>
+                <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"aria-current="page" >Home</a>
             </li>
             <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> About </a>
+                <a href="/about-us" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> About </a>
             </li>
             <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" > Services </a>
+                <a href="/contact-us" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" > Services </a>
             </li>
         </ul>
     )
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,7 +6,7 @@ export default function ErrorPage() {
                 <div class="mx-auto max-w-screen-sm text-center">
                     <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
 
-                    <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">We are already working to solve the problem. </p>
+                    <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Page not found. Go to Home page </p>
                 </div>
             </div>
         </section>
