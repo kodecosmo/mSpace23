235f8df9e04bd4604bca2bbabee91dc9415757b3
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2023-09-27 11:44:47 +0530

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,39 +1,14 @@
-import React from "react";
-
 export default function ErrorPage() {
-  return (
-    <section
-      className="relative h-screen bg-center bg-no-repeat bg-cover font-poppins"
-      style={{ backgroundImage: "url('https://i.postimg.cc/G3XTnb9s/4.jpg')" }}
-    >
-      <div className="absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50"></div>
-      <div className="flex items-center h-screen">
-        <div className="container relative justify-center px-4 mx-auto text-center">
-          <h1 className="inline-block mb-8 font-semibold text-gray-200 text-7xl lg:text-9xl">
-            Error 404
-          </h1>
-          <h2 className="mb-8 text-2xl font-semibold text-gray-100 lg:text-4xl">
-            Page not found
-          </h2>
-          <p className="mb-8 text-xl text-gray-200">
-            Sorry! we are unable to find the page that you are looking for...
-          </p>
-          <div className="flex flex-wrap items-center justify-center">
-            <a
-              href="/"
-              className="px-8 py-4 mb-4 mr-4 text-sm font-medium text-gray-100 bg-orange-600 rounded-full hover:bg-orange-800 md:w-auto"
-            >
-              Go homepage
-            </a>
-            <a
-              href="/"
-              className="px-8 py-4 mb-4 text-sm font-medium text-gray-100 bg-orange-600 rounded-full md:w-auto hover:bg-orange-800"
-            >
-              Try again
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+
+    return (
+        <section class="bg-white dark:bg-gray-900 flex h-screen">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 m-auto">
+                <div class="mx-auto max-w-screen-sm text-center">
+                    <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+
+                    <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Page not found. Go to Home page </p>
+                </div>
+            </div>
+        </section>
+    );
 }
