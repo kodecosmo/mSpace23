5fb90193d945ece97652e469c15fb2c08bf54cce
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2023-09-30 09:14:52 +0530

    Profile abs to rel. Slug changed

--- src/pages/tutors/[slug].jsx
+++ src/pages/tutors/[slug].jsx
@@ -10,7 +10,7 @@ export default function Tutors() {
       <NavbarMolecule />
       <main style={{ marginTop: "78px" }} className="w-full"></main>
       <TeacherProfile
-        teachername={router.query.slug}
+        teachername="Kaviru Hapuarachchi"
         teachernamedescription="This is the description about the teacher"
         subject="Chemistry"
         rating="5 out of 5"
